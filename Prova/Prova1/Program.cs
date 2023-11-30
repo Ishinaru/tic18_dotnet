1dc751b92550de2432316b7beb545488cd379a4c
Ishinaru <dvplima.cic@uesc.br>
2023-11-30 16:42:52 -0300

    commit 9

--- Prova/Prova1/Program.cs
+++ Prova/Prova1/Program.cs
@@ -15,9 +15,16 @@ AdicionarAdvogado(advogados, new Advogado("Pedro", new DateTime(8, 1, 2001), "12
 
 List<Cliente> clientes = new List<Cliente>();
 AdicionarCliente(clientes, new Cliente("João", new DateTime(1, 2, 1992), "12345678901", "Solteiro", "Programador"));
+AdicionarCliente(clientes, new Cliente("Maria", new DateTime(5, 12, 1972), "12345678902", "Casada", "Professora"));
+AdicionarCliente(clientes, new Cliente("José", new DateTime(13, 7, 1991), "12345678903", "Solteiro", "Médico"));
+AdicionarCliente(clientes, new Cliente("Ana", new DateTime(4, 4, 2004), "12345678904", "Solteira", "Estudante"));
+AdicionarCliente(clientes, new Cliente("Pedro", new DateTime(8, 1, 2001), "12345678905", "Solteiro", "Estudante"));
 
 
- static void AdicionarAdvogado(List<Advogado> advogados, Advogado advogado){
+
+
+
+static void AdicionarAdvogado(List<Advogado> advogados, Advogado advogado){
     if(advogados.Any(a => a.Cpf == advogado.Cpf || a.CNA == advogado.CNA)){
         throw new Exception("Advogado já cadastrado");
     }
@@ -26,9 +33,9 @@ AdicionarCliente(clientes, new Cliente("João", new DateTime(1, 2, 1992), "12345
 }
 
 static void AdicionarCliente(List<Cliente> clientes, Cliente cliente){
-    if(advogados.Any(a => a.Cpf == advogado.Cpf || a.CNA == advogado.CNA)){
-        throw new Exception("Advogado já cadastrado");
+    if(clientes.Any(c => c.Cpf == cliente.Cpf)){
+        throw new Exception("Cliente já cadastrado");
     }
     else
-        advogados.Add(advogado);
+        clientes.Add(cliente);
 }
