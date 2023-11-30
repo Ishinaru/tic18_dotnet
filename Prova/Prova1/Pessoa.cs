2b98cbf23290fc0d73a0e1c095d647b21cf85b69
Ishinaru <dvplima.cic@uesc.br>
2023-11-30 15:35:06 -0300

    commit1

---
+++ Prova/Prova1/Pessoa.cs
@@ -0,0 +1,8 @@
+using System.Globalization;
+CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("pt-BR");
+
+
+public abstract class Pessoa{
+    public string? Nome { get; set; }
+    Data
+}
---
+++ Semana4/Exercicio1/Veiculo.cs
@@ -0,0 +1,6 @@
+namespace Namespace;
+public class Veiculo{
+    public string? Modelo { get; set; }
+    public int Ano { get; set; }
+    public string? Cor { get; set; }
+}
