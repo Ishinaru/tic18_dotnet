6266cee2e5804980ab0f233688ab8191c03145ad
Ishinaru <dvplima.cic@uesc.br>
2023-11-30 16:39:57 -0300

    commit 8

--- Prova/Prova1/Program.cs
+++ Prova/Prova1/Program.cs
@@ -6,14 +6,26 @@ using Prova.Advogado;
 CultureInfo.CurrentCulture = CultureInfo.CreateSpecificCulture("pt-BR");
 
 List<Advogado> advogados = new List<Advogado>();
-var ad1('João', new DateTime(1990, 1, 1), '12345678901', '12345678901');
-var ad2('Maria', new DateTime(1990, 1, 1), '12345678902', '12345678902');
-var ad3('José', new DateTime(1990, 1, 1), '12345678903', '12345678903');
-var ad4('Ana', new DateTime(1990, 1, 1), '12345678904', '12345678904');
+AdicionarAdvogado(advogados, new Advogado("João", new DateTime(1, 2, 1992), "12345678901", "123456789"));
+AdicionarAdvogado(advogados, new Advogado("Maria", new DateTime(5, 12, 1972), "12345678902", "123456789"));
+AdicionarAdvogado(advogados, new Advogado("José", new DateTime(13, 7, 1991), "12345678903", "123456789"));
+AdicionarAdvogado(advogados, new Advogado("Ana", new DateTime(4, 4, 2004), "12345678904", "123456789"));
+AdicionarAdvogado(advogados, new Advogado("Pedro", new DateTime(8, 1, 2001), "12345678905", "123456789"));
+
 
 List<Cliente> clientes = new List<Cliente>();
+AdicionarCliente(clientes, new Cliente("João", new DateTime(1, 2, 1992), "12345678901", "Solteiro", "Programador"));
+
 
  static void AdicionarAdvogado(List<Advogado> advogados, Advogado advogado){
+    if(advogados.Any(a => a.Cpf == advogado.Cpf || a.CNA == advogado.CNA)){
+        throw new Exception("Advogado já cadastrado");
+    }
+    else
+        advogados.Add(advogado);
+}
+
+static void AdicionarCliente(List<Cliente> clientes, Cliente cliente){
     if(advogados.Any(a => a.Cpf == advogado.Cpf || a.CNA == advogado.CNA)){
         throw new Exception("Advogado já cadastrado");
     }
